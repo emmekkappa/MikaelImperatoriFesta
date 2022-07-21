c6071d822565439c0d2a7a1c4c4bbe852247edb7
emmekkappa <44437641+emmekkappa@users.noreply.github.com>
2022-07-21 09:50:55 +0100

    Update my-website.js

--- js/my-website.js
+++ js/my-website.js
@@ -55,7 +55,7 @@ $('.slider-icon').slick({
 });
 
 // Scrollify applied to homepage only through classes
-$(function () {
+/*$(function () {
 	$.scrollify({
 		section: ".homepage"
 	});
@@ -63,37 +63,19 @@ $(function () {
 $(function () {
 
 	$.scrollify({
-		// section should be an identifier that is the same for each section
-		// A selector for the sections.
 		section: ".homepage",
-		// Scrollify lets you define a hash value for each section.
-		// This makes it possible to permalink to particular sections.
-		// This is set as a data attribute on the sections.
-		// The name of the data attribute is defined by 'sectionName'.
 		sectionName: "section-name",
-		// A CSS selector for non-full-height sections, such as a header and footer.
 		interstitialSection: "",
-		// Define the easing method.
 		easing: "easeOutExpo",
-		// Scrolling speed in milliseonds
 		scrollSpeed: 1100,
-		// A distance in pixels to offset each sections position by.
 		offset: 0,
-		// A boolean to define whether scroll bars are visible or not.
 		scrollbars: true,
-		// Target container
 		target: "html,body",
-		// A string of selectors for elements that require standard scrolling behaviour.
 		standardScrollElements: false,
-		// A boolean to define whether Scollify assigns a height to the sections.
 		setHeights: true,
-		// Allows scrolling over overflowing content within sections
 		overflowScroll: true,
-		// Updates the browser location hash when scrolling through sections
 		updateHash: true,
-		// Allows to handle touch scroll events
 		touchScroll: true,
-		// callbacks
 		before: function () {},
 		after: function () {},
 		afterResize: function () {},
@@ -104,8 +86,9 @@ $(function () {
 
 $('.link').click(function () {
 	$.scrollify.move($(this).attr('href'))
-});
+});*/
 
+// Tooltip activation
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 });
@@ -127,6 +110,7 @@ $(document).scroll(function() {
 
 // Smooth scroll
 
+/*
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -136,6 +120,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+*/
 
 // Sliding underline nav
 
